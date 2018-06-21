d28dfe22b80bf92df81eee17946e0f722d6f5709
AlexFriedland <friedlandaj@gmail.com>
2018-06-21 12:06:08 +0200

    ActiveForm

---
+++ basic/views/site/entry.php
@@ -0,0 +1,23 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->label('Name') ?>
+
+    <?= $form->field($model, 'email')->label('Email') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+<?php ActiveForm::end(); ?>
+
+/* The view uses a powerful widget called ActiveForm to build the HTML form.
+The begin() and end() methods of the widget render the opening and closing form
+tags, respectively. Between the two method calls, input fields are created by
+the field() method. The first input field is for the "name" data, and the second
+for the "email" data. After the input fields, the yii\helpers\Html::submitButton()
+method is called to generate a submit button.
+*/
