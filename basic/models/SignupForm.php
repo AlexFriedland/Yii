cfbe72996bc7cac957787bb78f5c0e2db873f032
AlexFriedland <friedlandaj@gmail.com>
2018-07-31 16:24:46 +0200

    user signup form with permissions logic

---
+++ basic/models/SignupForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+use app\models\SignupForm;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * LoginForm is the model behind the login form.
+ *
+ * @property User|null $user This property is read-only.
+ *
+ */
+class SignupForm extends Model
+{
+
+}
+
+public function signup()
+{
+  if ($this->validate()) {
+    $user = new User();
+    $user->username = $this->username;
+    $user->email = $this->email;
+    $user->setPassword($this->password);
+    $user->generateAuthKey();
+    $user->save(false); //avoids validating twice
+
+    $auth = \Yii::$app->authManager;
+    $authorRole = $auth->getRole('author');
+    $auth->assign($authorRole, $user->getId());
+
+    return $user;
+  }
+  return null;
+}
+
+?>
